54c1325c09b1e15a3fcdb0dba200d4879358c045
bluescorpian <dsharryh27@gmail.com>
2022-11-11 22:05:59 +0200

    fix scripts

--- fill_stamina_unlock.lua
+++ fill_stamina_unlock.lua
@@ -1,8 +1,23 @@
+local utils = loadstring(game:HttpGet("https://raw.githubusercontent.com/bluescorpian/missingPieceUtils/main/missingPieceUtils.lua"))()
+
+local LocalPlayer = game:GetService("Players").LocalPlayer
 local FE = game:GetService("ReplicatedStorage"):WaitForChild("FE");
-local fillStaminaButton = game:GetService("Players").LocalPlayer.PlayerGui.GameGui.BottomLeft.Health.Stamina.Fill
 
-local fillStaminaClick = getconnections(fillStaminaButton.MouseButton1Down)[1].Function
-
-hookfunction(fillStaminaClick, function()
-    firesignal(FE.FillStaminaBought.OnClientEvent)
-end)
+function onSpawn()
+    local health = LocalPlayer.PlayerGui:WaitForChild("GameGui").BottomLeft.Health
+    local fillStaminaButton = health.Stamina:WaitForChild("Fill")
+    
+    -- try 30 times to see if event listener has been added and wait 1s
+    local success, attempts = utils.retryUntil(function() return #getconnections(fillStaminaButton.MouseButton1Down) ~= 0 end, 15, 2.5)
+    if not success then
+        error("Stamina never ran")
+    end
+    
+    local fillStaminaClick = getconnections(fillStaminaButton.MouseButton1Down)[1].Function
+    
+    hookfunction(fillStaminaClick, function()
+        firesignal(FE.FillStaminaBought.OnClientEvent)
+    end)
+end
+onSpawn()
+LocalPlayer.CharacterAdded:Connect(onSpawn)
--- instant_refuel_unlock_v2.lua
+++ instant_refuel_unlock_v2.lua
@@ -1,31 +1,34 @@
 local LocalPlayer = game:GetService("Players").LocalPlayer
 local FE = game:GetService("ReplicatedStorage"):WaitForChild("FE");
-local gameMenus = game:GetService("Players").LocalPlayer.PlayerGui.GameMenus
-local menuFunctions = require(gameMenus.MenuFunctions)
-local gasPumpGui = gameMenus:WaitForChild("Gas Pump")
-local instantButton = gasPumpGui:WaitForChild("BuyFrame"):WaitForChild("Instant")
 
 local maxFuel = 0
 local pump = nil
-
 FE.FuelPump.OnClientEvent:Connect(function(...)
-    local args = {...}
-    maxFuel = args[2]
-    pump = args[5]
+	local args = {...}
+	maxFuel = args[2]
+	pump = args[5]
 end)
+function onSpawn()
+	local gameMenus = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("GameMenus")
+	local menuFunctions = require(gameMenus.MenuFunctions)
+	local gasPumpGui = gameMenus:WaitForChild("Gas Pump")
+	local instantButton = gasPumpGui:WaitForChild("BuyFrame"):WaitForChild("Instant")
 
-local instantButtonClickHandler = getconnections(instantButton.MouseButton1Down)[1].Function
+	local instantButtonClickHandler = getconnections(instantButton.MouseButton1Down)[1].Function
 
-hookfunction(instantButtonClickHandler, function()
-    local buyGasResult = FE.BuyGas:InvokeServer(maxFuel, pump, false); -- <-- exploit here
-	if buyGasResult == "Good" then
-		getrenv()._G.CloseMenu("Small", gasPumpGui);
-		LocalPlayer.Character.Humanoid.WalkSpeed = 16;
-		return;
-	end;
-	if buyGasResult ~= "Gamepass" then
-		menuFunctions.ShowMessage("Error", (tostring(buyGasResult)));
-		return;
-	end;
-end)
+	hookfunction(instantButtonClickHandler, function()
+		local buyGasResult = FE.BuyGas:InvokeServer(maxFuel, pump, false); -- <-- exploit here
+		if buyGasResult == "Good" then
+			getrenv()._G.CloseMenu("Small", gasPumpGui);
+			LocalPlayer.Character.Humanoid.WalkSpeed = 16;
+			return;
+		end;
+		if buyGasResult ~= "Gamepass" then
+			menuFunctions.ShowMessage("Error", (tostring(buyGasResult)));
+			return;
+		end;
+	end)
+end
 
+onSpawn()
+LocalPlayer.CharacterAdded:Connect(onSpawn)
